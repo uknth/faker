31a2e7a70494313968022c6a3d199b8ad1b0499a
Ujjwal Kanth <uknth@outlook.com>
2018-12-19 03:11:33 +0530

    feat: filesource as input source

--- source.go
+++ source.go
@@ -1,6 +1,11 @@
 package faker
 
-import "github.com/pkg/errors"
+import (
+	"io/ioutil"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+)
 
 // Source provides an interface used to get the response
 type Source interface {
@@ -20,6 +25,27 @@ func NewTextSource(args map[string]string) (Source, error) {
 		return nil, errors.New("Insufficient Argument, missing \"content\"")
 	}
 	return &textSource{ct}, nil
+}
+
+type fileSource struct {
+	filepath string
+}
+
+func (fs *fileSource) Response() ([]byte, error) { return ioutil.ReadFile(fs.filepath) }
+
+// NewFileSource returns source which uses file as input
+func NewFileSource(args map[string]string) (Source, error) {
+	fp, ok := args["filepath"]
+	if !ok {
+		return nil, errors.New("Insufficient Argument, missing\"filepath\"")
+	}
+
+	abs, err := filepath.Abs(fp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &fileSource{abs}, nil
 }
 
 // NewSource returns text source
